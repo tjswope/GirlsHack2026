ecd6c5bb7bbec82150971ecfc94c0c3395fc2e84
jBellaria <jlbellaria@gmail.com>
2025-12-09 11:47:39 -0500

    Add files via upload

---
+++ Java/PracticeBlock.java
@@ -0,0 +1,46 @@
+import java.awt.Component;
+
+public class PracticeBlock extends Item {
+	private int value;
+	private int scale;
+	private boolean selected;
+	public PracticeBlock(int x_coordinate, int y_coordinate, int value, int scale) {
+		super(x_coordinate, y_coordinate, value + ".png/", scale);
+		this.value = value;
+		this.scale = scale;
+		this.selected = false;
+    }
+	
+	public void doubleValue() {
+		value *= 2;
+	}
+	
+	public int getValue() {
+		return value;
+	}
+	
+	public void setX(int x_coordinate) {
+    	this.x_coordinate = x_coordinate;
+    }
+    
+    public void setY(int y_coordinate) {
+    	this.y_coordinate = y_coordinate;
+    }
+    
+    public int getScale() {
+    	return scale;
+    }
+    
+    public void setSelected(boolean selected) {
+    	this.selected = selected;
+    }
+    
+    public boolean getSelected() {
+    	return selected;
+    }
+    
+    public void move(int[] dir){
+		y_coordinate += dir[0];
+		x_coordinate += dir[1];
+    }
+}
