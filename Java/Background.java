1bcf12c5ea6df4febed12e4b8a41ef992a9c1f7d
jBellaria <jlbellaria@gmail.com>
2026-01-30 14:22:23 -0500

    Update Background.java
    Merged Background & DesertBackground, fixed scale.

--- Java/Background.java
+++ Java/Background.java
@@ -1,16 +1,38 @@
 // Class: Background
 // Written by: Mr. Swope
 // Date: 1/27/2020
-// Description: This is an abstract class that provides partial implementation for a Background. You can't
-// 				create an instance of this class.
+// Description: This class provides the implementation for a DesertBackground. You probably won't want to modify this
+//              class.
+// Edited By: Jacqueline
+// Date: 01/30/2026
+// Description: folded DesertBackground & Background into the same class. Now provides the board for 2048.
+// 				I wouldn't modify this if I was you, but you may be able to change the image. Take note of the new
+// 				image's pixel width though, everything is based on that.
 import java.awt.Component;
 import java.awt.Graphics;
+import java.awt.Image;
+import java.net.URL;
 import javax.swing.ImageIcon;
 
-public abstract class Background {
-
+public class Background {
+	private int width; // default is 500 px
 	protected ImageIcon image;
 	protected int scale;
+	
+	public Background(int width) {
+		ClassLoader cldr = this.getClass().getClassLoader();	// These five lines of code load the background picture.
+		String imagePath = "images/board_2048.png";				// Change this line if you want to use a different 
+		URL imageURL = cldr.getResource(imagePath);				// background image.  The image should be saved in the
+		this.width = width;
+		
+		image = new ImageIcon(imageURL);
+		double scale = (this.width + 0.0) / image.getIconWidth();
+		int height = (int)(scale * image.getIconHeight());
+		Image scaled = image.getImage().getScaledInstance(this.width, 
+				height, Image.SCALE_SMOOTH);
+		
+		image = new ImageIcon(scaled);
+	}
 	
 	public void draw(Component c, Graphics g) {
 		image.paintIcon(c, g, 0, 0);
@@ -23,5 +45,4 @@ public abstract class Background {
 	public int getWidth() {
 		return image.getIconWidth();
 	}
-	
 }
